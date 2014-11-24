3f5da3b2662278dfc6c2787149b0d58fa0279627
Alexander Ward <alex.ward@bbqdigital.com>
2014-11-24 16:20:33 +0000

    added initial HTML and alex-avi file

---
+++ index.php
@@ -0,0 +1,159 @@
+<!doctype html>
+<html lang="en" prefix="og: http://ogp.me/ns#" class="no-js">
+<!-- TAG R1.2 - AW 300914 -->
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+    <meta name="revisit-after" content="7 days">
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,700,900,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="/static/styles/css/core.css" media="screen">
+    <link rel="stylesheet" href="/static/styles/css/print.css" media="print">
+    <link rel="shortcut icon" href="/static/images/logos/favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="/static/images/logos/apple-touch-icon.png" />
+    <link rel="apple-touch-icon" sizes="57x57" href="/static/images/logos/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="/static/images/logos/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="/static/images/logos/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="/static/images/logos/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon" sizes="120x120" href="/static/images/logos/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon" sizes="144x144" href="/static/images/logos/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="/static/images/logos/apple-touch-icon-152x152.png" />
+    <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script>window.jQuery || document.write('<script src="/static/scripts/lib/jquery.min.js"><\/script>')</script>
+    <!--[if lte IE 9]>
+        <script src="/assets/libraries/js/html5shiv.js"></script>
+    <![endif]-->
+    <script src="/static/scripts/lib/modernizr.min.js"></script>
+
+    <!-- SEO tags -->
+    <title>Building Better Quality | BBQ</title>
+
+    <link rel="canonical" href="https://bbqdigital.com/" />
+    <meta name="description" content="A full-service digital agency based in Southampton, Hampshire. Specialists in responsive web design. Building Better Quality websites for your customers" />
+    <meta property="og:locale" content="en_UK" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Building Better Quality | BBQ Digital" />
+    <meta property="og:description" content="A full-service digital agency based in Southampton, Hampshire. Specialists in responsive web design. Building Better Quality websites for your customers" />
+    <meta property="og:image" content="https://bbqdigital.com/static/images/logos/bbq-logo.jpg"/>
+    <meta property="og:url" content="https://bbqdigital.com/" />
+    <meta property="og:site_name" content="BBQ Digital" />
+</head>
+<body>
+    <div class="skip"><a href="#main">Skip to main content</a></div>
+    <section class="is-available">
+        <header>
+            <h1>I'm Alexander Ward. I design and build UX focused websites.</h1>
+        </header>
+        <div class="cta">
+            <p>I'm <strong>currently available</strong> for projects in the <strong>London</strong> area.</p>
+            <a href="" class="button">Get in touch</a>
+        </div>
+        <span class="scrolly">scrolly, scrolly!</span>
+    </section>
+    <section class="skills">
+        <h1>Skills to pay the bills</h1>
+        <p>I've been in the web industry for a long time (since 1998 to be exact) and over that time I've aquired a wide range of skills. </p>
+        <div class="skill-table">
+            <div class="col max-3">
+                <h2>Expert</h2>
+                <ul>
+                    <li>HTML/CSS3</li>
+                    <li>SASS/LESS</li>
+                    <li>jQuery</li>
+                    <li>Responsive Design</li>
+                    <li>Accessibility</li>
+                    <li>Procedural PHP</li>
+                    <li>Git/SVN</li>
+                </ul>
+            </div>
+            <div class="col max-3">
+                <h2>Middle-weight</h2>
+                <ul>
+                    <li>OO PHP</li>
+                    <li>MySQL</li>
+                    <li>Pure JavaScript</li>
+                    <li>Build Systems (e.g. Jenkins)</li>
+                    <li>Server Management</li>
+                    <li>CMS Templating (Perch &amp; Wordpress)</li>
+                </ul>
+            </div>
+            <div class="col max-3">
+                <h2>Competent Beginner</h2>
+                <ul>
+                    <li>Node.js</li>
+                    <li>Symfont 2 framework</li>
+                    <li>CMS Templating (Drual, EpiServer, Umbraco)</li>
+                    <li>Illustration</li>
+                    <li>3D Modelling</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <section class="brands">
+        <h2>I love the brands I've worked with &hellip;</h2>
+        <p>I've been fortunate enough to work with some amazing brands over the years.</p>
+        <p>Here are a few of my favourites:</p>
+        <ul class="brand-grid">
+            <li>SSE</li>
+            <li>B&amp;Q</li>
+            <li>Honda</li>
+            <li>Everest</li>
+            <li>Bowmore</li>
+            <li>P&amp;G</li>
+            <li>Pampers</li>
+            <li>Iams</li>
+            <li>Plusnet</li>
+        </ul>
+    </section>
+    <section class="references">
+        <h2>&hellip; and I think they love me too!</h2>
+        <ul class="quotes">
+            <li>
+                <blockquote itemprop="review">
+                    <span class="hide" itemprop="itemreviewed">BBQ Digital</span>
+                    <strong itemprop="description">"Alex clearly has a great level of experience and technical knowledge and completed the task at ease. His enthusiasm and easy going attitude also made the task that much easier."</strong>
+                    <br> - <span itemprop="reviewer">Spencer Leah, Head of Digital @ Plusnet</span>
+                </blockquote>
+            </li>
+            <li>
+                <blockquote itemprop="review">
+                    <span class="hide" itemprop="itemreviewed">BBQ Digital</span>
+                    <strong itemprop="description">"Alex's deep experience in this area is evident both in the quality of his work, and his clear articulation of problems/challenges alongside workable solutions. I would happily recommend Alex and BBQ Digital, and would certainly use them again."</strong>
+                    <br> - <span itemprop="reviewer">David Fowler, Business &amp; Systems Integration Manager @ Accenture</span>
+                </blockquote>
+            </li>
+            <li>
+                <blockquote itemprop="review">
+                    <span class="hide" itemprop="itemreviewed">BBQ Digital</span>
+                    <strong itemprop="description">"Alex has been nothing short of exemplary in his work. He has taken every brief and task given, always going beyond what was asked to deliver top quality results in a timely and professional manner."</strong>
+                    <br> -<span itemprop="reviewer"> Vincent Pickering, Lead UX developer @ Bolser</span>
+                </blockquote>
+            </li>
+            <li>
+                <blockquote itemprop="review">
+                    <span class="hide" itemprop="itemreviewed">BBQ Digital</span>
+                    <strong itemprop="description">"Alex is a solid front end developer and a pleasure to work with. I was fortunate enough to work with him on a number of projects for high profile clients including P&amp;G. He has a great attitude to development challenges and always delivers. I would certainly work with him again."</strong>
+                    <br> - <span itemprop="reviewer">Phillip Mundy, Disruptive technology entrepreneur</span>
+                </blockquote>
+            </li>
+        </ul>
+    </section>
+    <section class="contact">
+        <h2>Get in touch!</h2>
+        <div class="col max-2">
+            <p>I’ve recently moved to the amazing city of London and I’m currently available for contract work. If you wish to hire me for a contract or  project, please contact me either using the form or the details below.</p>
+            <p>
+                <strong>Telephone:</strong> 07538 943004
+                <strong>Email:</strong> <a href="mailto:alex@alexward.me.uk">alex@alexward.me.uk</a>
+                <strong>Skype:</strong> alexward1981
+                <strong>Carrier Pigeon:</strong> No longer available.
+            </p>
+        </div>
+        <p><strong>Notice to agents:</strong> Please don’t contact me for permanent roles, I love contracting and no power in the verse will take me away from it.</p>
+    </section>
+    <footer>
+        &copy;MMIV Alexander Ward and BBQ Digital Limited. All Rights reserved.<br />
+        Registered in England &amp; Wales 08480746.
+    </footer>
+</body>
+</html>
