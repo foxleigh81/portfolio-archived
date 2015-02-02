96b0a8c3f3aff3ee26b0b4f6ab80a01fdc6ee4df
Alexander Ward <alex.ward@bbqdigital.com>
2015-02-02 17:34:55 +0000

    added robots.txt and htaccess file. Created an error page and converted header and footer to php includes

---
+++ includes/footer.php
@@ -0,0 +1,25 @@
+    <footer class="pinned">
+        <div class="info">
+            <p>&copy;MMV Alexander Ward and BBQ Digital Limited. All Rights reserved.</p>
+            <p>Registered in England &amp; Wales 08480746.</p>
+        </div>
+        <div class="links">
+           <ul>
+               <li>
+                   <a href="index.php">Home</a> |
+               </li>
+               <li>
+                   <a href="roadmap.php">Development Roadmap</a> |
+               </li>
+               <li>
+                   <a href="http://lxword.com">Blog</a> |
+               </li>
+               <li>
+                   <a href="https://www.dropbox.com/s/arxwap21eqkw2h3/AlexWard-ContractorCV.docx?dl=0">Download my CV</a>
+               </li>
+           </ul>
+        </div>
+    </footer>
+    <script src="static/scripts/core.js"></script>
+</body>
+</html>
